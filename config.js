06f0e8e090894e5a6e814dd5191b26e3e2c04732
KAVEESHA SITHUM <107849120+Kaveeshasithum@users.noreply.github.com>
2022-09-30 13:10:19 +0530

    ⚜️𝘋𝘈𝘙𝘒 𝘕𝘌𝘙𝘖 𝘔𝘋⚜️ setting.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ global.alivemsg =`HI I'M  ⚜️𝘋𝘈𝘙𝘒 𝘕𝘌𝘙𝘖 𝘔𝘋⚜️
 
 
 
-global.alivepic = "https://i.ibb.co/8N6fR4X/Whats-App-Image-2022-09-17-at-21-22-46.jpg" //you logo key 
+global.alivepic = "https://i.ibb.co/2d6vv85/Whats-App-Image-2022-09-26-at-07-59-53.jpg" //you logo key 
 
 
 global.ownername = " KAVEESHA " //ur owner name
